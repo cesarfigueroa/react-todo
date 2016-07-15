149956ca6a4d167329fbd3f23f690cc795694bf3
Cesar Figueroa <cesar.figueroa@me.com>
2016-07-15 13:18:51 -0500

    Condense state setup in reducer test

--- test/reducers/tasks.spec.js
+++ test/reducers/tasks.spec.js
@@ -8,17 +8,16 @@ describe('tasks reducer', function() {
 
   describe('ADD_TASK', function() {
     it('adds a task', function() {
-      expect(reducer([], {
+      let currentState = [];
+      let newState = [
+        { id: 1, title: 'Task 1', isComplete: false }
+      ];
+
+      expect(reducer(currentState, {
         type: 'ADD_TASK',
         id: 1,
         title: 'Task 1'
-      })).to.eql([
-        {
-          id: 1,
-          title: 'Task 1',
-          isComplete: false
-        }
-      ]);
+      })).to.eql(newState);
     });
   });
 
@@ -26,44 +25,26 @@ describe('tasks reducer', function() {
     context('given an existing task id', function() {
       it('toggles the task', function() {
         let currentState = [
-          {
-            id: 1,
-            title: 'Task 1',
-            isComplete: false
-          },
-          {
-            id: 2,
-            title: 'Task 2',
-            isComplete: false
-          }
+          { id: 1, title: 'Task 1', isComplete: false },
+          { id: 2, title: 'Task 2', isComplete: false }
+        ];
+
+        let newState = [
+          { id: 1, title: 'Task 1', isComplete: false },
+          { id: 2, title: 'Task 2', isComplete: true }
         ];
 
         expect(reducer(currentState, {
           type: 'TOGGLE_TASK',
           id: 2
-        })).to.eql([
-          {
-            id: 1,
-            title: 'Task 1',
-            isComplete: false
-          },
-          {
-            id: 2,
-            title: 'Task 2',
-            isComplete: true
-          }
-        ]);
+        })).to.eql(newState);
       });
     });
 
     context('given a missing task id', function() {
       it('returns the current state', function() {
         let currentState = [
-          {
-            id: 1,
-            title: 'Task 1',
-            isComplete: false
-          }
+          { id: 1, title: 'Task 1', isComplete: false }
         ];
 
         expect(reducer(currentState, {
