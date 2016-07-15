c849ba31bdb30809114b09344969de5d8eb4a843
Cesar Figueroa <cesar.figueroa@me.com>
2016-07-15 12:43:12 -0500

    Setup uglifyjs
    NODE_ENV=production is required to extract the production version of
    React

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@
 const del = require('del');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
 const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
 const WebpackDevServer = require('webpack-dev-server');
 
 gulp.task('assets:clean', () => del.sync(['dist']));
@@ -19,19 +21,18 @@ gulp.task('assets:stylesheets:watch', () => {
 });
 
 gulp.task('assets:javascripts', ['assets:clean'], callback => {
-  webpack(require('./webpack.config.js'), callback);
+  webpack(webpackConfig, callback);
+});
+
+gulp.task('assets:javascripts:compress', ['assets:javascripts'], () => {
+  return gulp.src('dist/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('assets:javascripts:watch', () => {
-  let compiler = webpack(require('./webpack.config.js'));
-
-  new WebpackDevServer(compiler, {
-    publicPath: '/dist',
-    historyApiFallback: true,
-    stats: {
-      chunks: false
-    }
-  }).listen(8080, 'localhost');
+  new WebpackDevServer(webpack(webpackConfig), webpackConfig.devServer)
+    .listen(8080, 'localhost');
 });
 
 gulp.task('assets:watch', [
@@ -44,5 +45,6 @@ gulp.task('assets:watch', [
 gulp.task('assets', [
   'assets:clean',
   'assets:stylesheets',
-  'assets:javascripts'
+  'assets:javascripts',
+  'assets:javascripts:compress'
 ]);
