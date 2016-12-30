2844a27517336fb3a40c318df0558ed63250f9de
Cesar Figueroa <cesar.figueroa@me.com>
2016-12-30 12:28:54 -0600

    Make TaskForm input controlled

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -2,27 +2,37 @@ import React, { Component } from 'react';
 import { v4 as uuid } from 'node-uuid';
 
 class TaskForm extends Component {
-  onKeyDown(event) {
-    let value = this.input.value.trim();
+  constructor() {
+    super();
 
-    if (value && event.key == 'Enter') {
+    this.state = {
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  handleKeyDown(event) {
+    const value = event.target.value;
+    const hasValue = value.trim() !== '';
+
+    if (hasValue && event.key === 'Enter') {
       this.props.addTask(uuid(), value);
-      this.resetField();
+      this.setState({ value: '' });
     }
   }
-
-  resetField() {
-    this.input.value = '';
-  }
-
   render() {
     return (
       <input
         className="taskForm"
         type="text"
         placeholder="What would you like to get done?"
-        ref={node => this.input = node}
-        onKeyDown={event => this.onKeyDown(event)}
+        value={this.state.value}
+        onChange={this.handleChange}
+        onKeyDown={this.handleKeyDown}
       />
     );
   }
